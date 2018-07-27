1571e2fc974e2e44b2b3e71b2bf48ed8bd8343d1
gokugml <32604536+gokugml@users.noreply.github.com>
2018-07-27 00:15:38 -0700

    fix gui bug and upload engine.r

---
+++ engine.r
@@ -0,0 +1,67 @@
+# Clean Data 
+# 1.set H or A; 2. set date format; 3. calculate the 'Four Factors' difference between the team and its opponent.
+# table is in the format of "TeamName_0"
+cleanTable <- function(table){
+  newtable <- table
+  newtable$HorA <- ifelse(newtable$HorA == '', 'H','A')
+  newtable$Date <- as.Date(newtable$Date,format = '%m/%d/%Y')
+  newtable$diff_eFG <- (newtable$`eFG%`-newtable$`OppeFG%`) * 100
+  newtable$diff_TOV <- newtable$`TOV%`-newtable$`OppTOV%`
+  newtable$diff_ORB <- newtable$`ORB%`-newtable$`OppDRB%`
+  newtable$diff_FT <- (newtable$`FT/FGA`-newtable$`OppFT/FGA`) * 100
+  newtable$diff_score <- newtable$Tm - newtable$Opp_1
+  return(newtable)
+} 
+
+# Linear regression: diff_score ~ Four factors. Ideally we need to train with as much data as possible
+# As a demo, I trained the model with only two teams' data
+trainModel <- function(newtables){
+  fit <- lm(diff_score ~ diff_eFG + diff_TOV + diff_ORB + diff_FT, data = newtables)
+  return(fit$Coefficients)
+}
+
+# Calculate Team's past performance, filtered on date, teamname and HomeOrAway
+calTeamPastStats <- function(date,newtable,HorA){
+  a<-list()
+  subdf <- newtable[newtable$Date < as.Date(date,format = '%m/%d/%Y') & newtable$HorA == HorA,]
+  a$normPar_eFG <- c(mean(subdf$diff_eFG), var(subdf$diff_eFG))
+  a$normPar_TOV <- c(mean(subdf$diff_TOV), var(subdf$diff_TOV))
+  a$normPar_ORB <- c(mean(subdf$diff_ORB), var(subdf$diff_ORB))
+  a$normPar_FT <- c(mean(subdf$diff_FT), var(subdf$diff_FT))
+  return(a)
+}
+
+# Simulation
+param_list <- trainModel(temp)
+simulation <- function(date,HomeTeam,AwayTeam){
+  statsH <- calTeamPastStats(date,HomeTeam,'H')
+  statsA <- calTeamPastStats(date,AwayTeam,'A')
+  eFG <- rnorm(100,mean = statsH$normPar_eFG[1], sd = sqrt(statsH$normPar_eFG[2])) - rnorm(100,mean = statsA$normPar_eFG[1], sd = sqrt(statsA$normPar_eFG[2]))
+  TOV <- rnorm(100,mean = statsH$normPar_TOV[1], sd = sqrt(statsH$normPar_TOV[2])) - rnorm(100,mean = statsA$normPar_TOV[1], sd = sqrt(statsA$normPar_TOV[2]))
+  ORB <- rnorm(100,mean = statsH$normPar_ORB[1], sd = sqrt(statsH$normPar_ORB[2])) - rnorm(100,mean = statsA$normPar_ORB[1], sd = sqrt(statsA$normPar_ORB[2]))
+  FT <- rnorm(100,mean = statsH$normPar_FT[1], sd = sqrt(statsH$normPar_FT[2])) - rnorm(100,mean = statsA$normPar_FT[1], sd = sqrt(statsA$normPar_FT[2]))
+  pred_scorediff <- param_list[1] + param_list[2] * eFG + param_list[3]* TOV + param_list[4]* ORB + param_list[5] * FT
+  winRatio_H <- sum(pred_scorediff>0)/100
+  return(winRatio_H)
+}
+
+# Return T or F to tell whether to place the bets
+BetorNot <- function(winRatio_H, marketOdds){
+  key = F
+  EV <- winRatio_H*marketOdds - (1-winRatio_H)
+  if(EV>0.05){
+    key = T
+  }
+  return(key)
+}
+
+# Return the balance after knowing the betting results
+pnl <- function(balance, betAmount, marketOdds, gameResult){
+  if(gameResult == 'W'){
+    profit <- betAmount * marketOdds
+  }
+  else{
+    profit <- -betAmount
+  }
+  balance <- balance + profit
+}
--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ Created on Tue Jul 24 20:35:52 2018
 """
 
 import tkinter as tk
+#import ball_game
 
 
 if __name__ == '__main__':
@@ -14,9 +15,28 @@ if __name__ == '__main__':
     window.title('SW600 Final project')
     window.geometry('1000x1000')
     window.resizable(width=True,height=True)
-    
-tk.Label(window,text='Basketball Game Predictor',font=('Arial',20),bg='CORNFLOWERBLUE',
-         width=100,height=3).pack()
+
+def bet():
+    """
+    1. set team based on v
+
+    2. use team name and date select team from dataframe
+
+
+    :return:
+    """
+    try:
+        if v.get() == 1:
+            team = home_team.get()
+        elif v.get() == 2:
+            team = away_team.get()
+        print team
+    except tk.TclError:
+        return
+
+
+tk.Label(window, text='Basketball Game Predictor', font=('Arial', 20), bg='CORNFLOWERBLUE',
+         width=100, height=3).pack()
 frm = tk.Frame(window)
 frm.pack()
 
@@ -26,41 +46,44 @@ frm_b = tk.Frame(frm)
 frm_t.pack(side='top')
 frm_b.pack(side='bottom')
 
-l1=tk.Label(frm_t,text='Data',fg='white',bg='black',font=('Arial',18),
-            width=10).grid(row=0,column=0)
-l2=tk.Label(frm_t,text='Home Team',fg='white',bg='black',font=('Arial',18),
-            width=10).grid(row=1,column=0)
-l3=tk.Label(frm_t,text='Away Team',fg='white',bg='black',font=('Arial',18),
-            width=10).grid(row=2,column=0)
-l4=tk.Label(frm_t,text='Balance',fg='white',bg='black',font=('Arial',18),
-            width=10).grid(row=0,column=2)
-l5=tk.Label(frm_t,text='Mkt Odds',fg='white',bg='black',font=('Arial',18),
-            width=10).grid(row=1,column=2)
-l6=tk.Label(frm_t,text='My Odds',fg='white',bg='black',font=('Arial',18),
-            width=10).grid(row=2,column=2)
-l7=tk.Label(frm_t,text='Bet Amount',fg='white',bg='black',font=('Arial',18),
-            width=10).grid(row=0,column=4)
-e1 = tk.StringVar()
-e2 = tk.StringVar()
-e3 = tk.StringVar()
-e4 = tk.StringVar()
-e5 = tk.StringVar()
-e6 = tk.StringVar()
-entry1=tk.Entry(frm_t,textvariable = e1).grid(row=0,column=1)
-entry2=tk.Entry(frm_t,textvariable = e2).grid(row=1,column=1)
-entry3=tk.Entry(frm_t,textvariable = e3).grid(row=2,column=1)
-entry4=tk.Entry(frm_t,textvariable = e4).grid(row=0,column=3)
-entry5=tk.Entry(frm_t,textvariable = e5).grid(row=1,column=3)
-entry6=tk.Entry(frm_t,textvariable = e6).grid(row=2,column=3)
-v=tk.StringVar()
-r1=tk.Radiobutton(frm_t, text='Bet H',variable=v).grid(row=1,column=4)
-r2=tk.Radiobutton(frm_t,text='Bet A',variable=v).grid(row=2,column=4)
+label_date = tk.Label(frm_t, text='Date', fg='white', bg='black', font=('Arial', 18),
+                      width=10).grid(row=0, column=0)
+label_home_team = tk.Label(frm_t, text='Home Team', fg='white', bg='black', font=('Arial', 18),
+                           width=10).grid(row=1, column=0)
+label_away_team = tk.Label(frm_t, text='Away Team', fg='white', bg='black', font=('Arial', 18),
+                           width=10).grid(row=2, column=0)
+label_balance = tk.Label(frm_t, text='Balance', fg='white', bg='black', font=('Arial', 18),
+                         width=10).grid(row=0, column=2)
+label_mkt_odds = tk.Label(frm_t, text='Market Odds', fg='white', bg='black', font=('Arial', 18),
+                          width=10).grid(row=1, column=2)
+label_my_odds = tk.Label(frm_t, text='My Odds', fg='white', bg='black', font=('Arial', 18),
+                         width=10).grid(row=2, column=2)
+label_bet_amount = tk.Label(frm_t, text='Bet Amount', fg='white', bg='black', font=('Arial', 18),
+                            width=10).grid(row=0, column=4)
+date = tk.StringVar()
+home_team = tk.StringVar()
+away_team = tk.StringVar()
+balance = tk.StringVar()
+balance = tk.StringVar()
+mkt_odds = tk.StringVar()
+my_odds = tk.StringVar()
+bet_amount = tk.StringVar()
 
-b=tk.Button(frm_b,text='Bet',fg='red',font=('Arial',18),
-            width=10,command=bet_result).pack(side='top')     
-t = tk.Text(frm_b,width=50,height=20)
+txt_date = tk.Entry(frm_t, textvariable=date).grid(row=0, column=1, sticky=tk.W)
+txt_home_team = tk.Entry(frm_t, textvariable=home_team).grid(row=1, column=1, sticky=tk.W)
+txt_away_team = tk.Entry(frm_t, textvariable=away_team).grid(row=2, column=1, sticky=tk.W)
+txt_balance = tk.Entry(frm_t, textvariable=balance).grid(row=0, column=3, sticky=tk.W)
+txt_mkt_odds = tk.Entry(frm_t, textvariable=mkt_odds).grid(row=1, column=3, sticky=tk.W)
+txt_my_odds = tk.Entry(frm_t, textvariable=my_odds).grid(row=2, column=3, sticky=tk.W)
+txt_bet_amount = tk.Entry(frm_t, textvariable=bet_amount).grid(row=0, column=5, sticky=tk.W)
+
+v = tk.IntVar()
+r1 = tk.Radiobutton(frm_t, text='Bet Home', variable=v, value=1, indicatoron=0).grid(row=1, column=4)
+r2 = tk.Radiobutton(frm_t, text='Bet Away', variable=v, value=2, indicatoron=0).grid(row=2, column=4)
+
+b = tk.Button(frm_b, text='Bet', fg='red', font=('Arial', 18),
+              width=10, command=bet).pack(side='top')
+t = tk.Text(frm_b, width=50, height=20)
 t.pack(fill=tk.X, expand=1)
-
-
 
 tk.mainloop()
